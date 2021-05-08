8dacdea3dbbc7e55740d769d7762824359bccaee
TheSilkMiner <thesilkminer@outlook.com>
2021-05-08 10:34:27 +0200

    Mark dependency on required mods
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/java/mods/thecomputerizer/dimensionhoppertweaks/DimensionHopperTweaks.java
+++ src/main/java/mods/thecomputerizer/dimensionhoppertweaks/DimensionHopperTweaks.java
@@ -5,12 +5,13 @@ import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = DimensionHopperTweaks.MODID, name = DimensionHopperTweaks.NAME, version = DimensionHopperTweaks.VERSION)
+@Mod(modid = DimensionHopperTweaks.MODID, name = DimensionHopperTweaks.NAME, version = DimensionHopperTweaks.VERSION, dependencies = DimensionHopperTweaks.DEPENDENCIES)
 public class DimensionHopperTweaks
 {
     public static final String MODID = "dimensionhoppertweaks";
     public static final String NAME = "Dimension Hopper Tweaks";
     public static final String VERSION = "1.0";
+    public static final String DEPENDENCIES = "required-after:forge@[14.23.5.2847,);required-after:fermion;required-after:avaritia;";
 
     @Mod.Instance(MODID)
     public static DimensionHopperTweaks INSTANCE;
