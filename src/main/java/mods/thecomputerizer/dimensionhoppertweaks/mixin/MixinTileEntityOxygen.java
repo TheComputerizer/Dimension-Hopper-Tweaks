047a98d231b340a2230f9d1f4af4643161101552
TheComputerizer <dcpixelation42@gmail.com>
2022-07-19 12:30:26 -0500

    mixin fix

--- src/main/java/mods/thecomputerizer/dimensionhoppertweaks/mixin/MixinTileEntityOxygen.java
+++ src/main/java/mods/thecomputerizer/dimensionhoppertweaks/mixin/MixinTileEntityOxygen.java
@@ -4,7 +4,9 @@ import mekanism.common.capabilities.Capabilities;
 import micdoodle8.mods.galacticraft.core.energy.EnergyUtil;
 import micdoodle8.mods.galacticraft.core.energy.tile.TileBaseElectricBlock;
 import micdoodle8.mods.galacticraft.core.tile.TileEntityOxygen;
+import micdoodle8.mods.galacticraft.core.wrappers.FluidHandlerWrapper;
 import micdoodle8.mods.galacticraft.planets.mars.tile.TileEntityElectrolyzer;
+import micdoodle8.mods.galacticraft.planets.mars.tile.TileEntityMethaneSynthesizer;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
@@ -22,25 +24,16 @@ public abstract class MixinTileEntityOxygen extends TileBaseElectricBlock {
 
     @Inject(at = @At(value = "HEAD"), method = "hasCapability", cancellable = true)
     private void hasCapability(Capability<?> capability, EnumFacing facing, CallbackInfoReturnable<Boolean> cir) {
-        if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY) {
-            cir.setReturnValue(true);
-        } else {
-            if (capability == Capabilities.GAS_HANDLER_CAPABILITY)
-                cir.setReturnValue(Capabilities.GAS_HANDLER_CAPABILITY.cast((TileEntityOxygen)(Object)this));
-            else cir.setReturnValue(super.hasCapability(capability,facing));
-        }
+        if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY) cir.setReturnValue(true);
+        else cir.setReturnValue(super.hasCapability(capability, facing));
         cir.cancel();
     }
 
     @Inject(at = @At(value = "HEAD"), method = "getCapability", cancellable = true)
     private void getCapability(Capability<?> capability, EnumFacing facing, CallbackInfoReturnable<Object> cir) {
-        if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY) {
-            cir.setReturnValue(true);
-        } else {
-            if (capability == Capabilities.GAS_HANDLER_CAPABILITY)
-                cir.setReturnValue(Capabilities.GAS_HANDLER_CAPABILITY.cast((TileEntityOxygen)(Object)this));
-            else cir.setReturnValue(super.getCapability(capability,facing));
-        }
+        if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY)
+            cir.setReturnValue(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY.cast(new FluidHandlerWrapper((TileEntityOxygen)(Object)this, facing)));
+        else cir.setReturnValue(super.getCapability(capability,facing));
         cir.cancel();
     }
 }
