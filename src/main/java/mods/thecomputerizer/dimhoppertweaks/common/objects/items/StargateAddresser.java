96a6d29fb255ff9d5a3ca11f98c96cb2660a1070
TheComputerizer <dcpixelation42@gmail.com>
2023-01-08 20:03:37 -0600

    clean up stargate addresser

--- src/main/java/mods/thecomputerizer/dimhoppertweaks/common/objects/items/StargateAddresser.java
+++ src/main/java/mods/thecomputerizer/dimhoppertweaks/common/objects/items/StargateAddresser.java
@@ -18,57 +18,67 @@ import net.minecraft.world.World;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraft.init.Items;
 
-public class StargateAddresser extends EpicItem {
-    public ITextComponent text;
-    public Stargate gate = new Stargate();
-    public int id = 0;
-    public int[] from = {-28,-29,0,-31,-1502,-30,816,-15,-1500,-1501,-1506,-16,-1508,-1509,-1510,66,-19};
-    public int[] to = {-29,-30,-31,-13,-1503,-1502,-15,-1500,-1501,-1506,-16,-1508,-1509,-1510,-17,-19,-21};
-    public boolean found = false;
+import javax.annotation.Nonnull;
+import java.util.HashMap;
 
-    public StargateAddresser() {}
+public class StargateAddresser extends EpicItem {
+    public final Stargate gate = new Stargate();
+    public final HashMap<Integer, Integer> dimIDPairs = new HashMap<>();
+
+    public StargateAddresser() {
+        dimIDPairs.put(-28,-29);    //moon->mars
+        dimIDPairs.put(-29,-30);    //mars->asteroids
+        dimIDPairs.put(0,-31);      //overworld->venus
+        dimIDPairs.put(-31,-13);    //venus->mercury
+        dimIDPairs.put(-1502,-1503);//phobos->deimos
+        dimIDPairs.put(-30,-1502);  //asteroids->phobos
+        dimIDPairs.put(816,-15);    //lunalus->jupiter
+        dimIDPairs.put(-15,-1500);  //jupiter->io
+        dimIDPairs.put(-1500,-1501);//io->europa
+        dimIDPairs.put(-1501,-1506);//europa->ganymede
+        dimIDPairs.put(-1506,-16);  //ganymede->saturn
+        dimIDPairs.put(-16,-1508);  //saturn->rhea
+        dimIDPairs.put(-1508,-1509);//rhea->oberon
+        dimIDPairs.put(-1509,-1510);//oberon->titania
+        dimIDPairs.put(-1510,-17);  //titania->uranus
+        dimIDPairs.put(66,-19);     //erebus->pluto
+        dimIDPairs.put(-19,-21);    //pluto->eris
+    }
 
     @Override
-    public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player,EnumHand hand) {
+    @Nonnull
+    public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, @Nonnull  EnumHand hand) {
         player.getCooldownTracker().setCooldown(this, 200);
         ItemStack stack = new ItemStack(Items.PAPER);
-        ItemStack chev = new ItemStack(SGCraft.sgChevronUpgrade);
-        text = new TextComponentString("Gate successfully located with address: (empty address)!");
+        EnumActionResult result = EnumActionResult.PASS;
         if (!world.isRemote) {
-            for (int i=0;i<from.length;i++) {
-                if (world.provider.getDimension() == from[i]) {
-                    id = to[i];
-                    found = true;
-                }
-            }
-            if (!found) {
-                text = new TextComponentString("Gate could not be located");
-                player.sendStatusMessage(text, true);
-                return new ActionResult<>(EnumActionResult.SUCCESS, stack);
-            }
-            DimensionManager.initDimension(id);
-            World genhere = DimensionManager.getWorld(id);
-            BlockPos pos = player.getPosition();
-            gate.build(genhere, pos);
-            Block block = genhere.getBlockState(pos).getBlock();
-            if (block instanceof ISGBlock) {
-                SGBaseTE te = ((ISGBlock) block).getBaseTE(genhere, pos);
-                if (te != null) {
-                    te.applyChevronUpgrade(chev, player);
-                    String address;
-                    try {
-                        address = te.getHomeAddress();
-                    } catch (SGAddressing.AddressingError e) {
-                        player.sendStatusMessage(text, true);
-                        return null;
+            ItemStack chev = new ItemStack(SGCraft.sgChevronUpgrade);
+            ITextComponent text = new TextComponentString("Gate could not be located");
+            int id = world.provider.getDimension();
+            if(dimIDPairs.containsKey(id)) {
+                DimensionManager.initDimension(dimIDPairs.get(id));
+                World genhere = DimensionManager.getWorld(dimIDPairs.get(id));
+                BlockPos pos = player.getPosition();
+                gate.build(genhere, pos);
+                Block block = genhere.getBlockState(pos).getBlock();
+                if (block instanceof ISGBlock) {
+                    SGBaseTE te = ((ISGBlock) block).getBaseTE(genhere, pos);
+                    if (te != null) {
+                        te.applyChevronUpgrade(chev, player);
+                        String address;
+                        try {
+                            address = te.getHomeAddress();
+                            text = new TextComponentString("Gate successfully located with address " + address + "!");
+                            stack.setStackDisplayName("§5" + address);
+                            result = EnumActionResult.SUCCESS;
+                        } catch (SGAddressing.AddressingError e) {
+                            result = EnumActionResult.FAIL;
+                        }
                     }
-                    text = new TextComponentString("Gate successfully located with address: " + address + "!");
-                    stack.setStackDisplayName("§5" + address);
                 }
             }
             player.sendStatusMessage(text, true);
-            found=false;
         }
-        return new ActionResult<>(EnumActionResult.SUCCESS, stack);
+        return new ActionResult<>(result, stack);
     }
 }
