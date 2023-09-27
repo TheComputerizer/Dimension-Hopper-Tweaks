71cf117c63609ed66d45be64893b9d9e65887890
TheComputerizer <dcpixelation42@gmail.com>
2023-09-27 14:09:04 -0500

    Patch for silent gems guidebook crash

---
+++ src/main/java/mods/thecomputerizer/dimhoppertweaks/mixin/mods/MixinGuideBookUtils.java
@@ -0,0 +1,21 @@
+package mods.thecomputerizer.dimhoppertweaks.mixin.mods;
+
+import net.minecraft.item.ItemStack;
+import net.silentchaos512.lib.guidebook.GuideBook;
+import net.silentchaos512.lib.guidebook.IGuidePage;
+import net.silentchaos512.lib.guidebook.misc.GuideBookUtils;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Objects;
+
+@Mixin(value = GuideBookUtils.class, remap = false)
+public class MixinGuideBookUtils {
+
+    @Inject(at = @At("HEAD"), method = "findFirstPageForStack", cancellable = true)
+    private static void dimhoppertweaks$findFirstPageForStack(GuideBook book, ItemStack stack, CallbackInfoReturnable<IGuidePage> cir) {
+        if(Objects.isNull(stack)) cir.setReturnValue(null);
+    }
+}
