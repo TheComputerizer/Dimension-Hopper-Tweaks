9681d2d5d6759ca40f2b994347cea26eb8c25d03
TheComputerizer <dcpixelation42@gmail.com>
2023-09-27 21:49:13 -0500

    Remove even more unnecessary warning/error log spam

---
+++ src/main/java/mods/thecomputerizer/dimhoppertweaks/mixin/mods/MixinMutableLootTable.java
@@ -0,0 +1,16 @@
+package mods.thecomputerizer.dimhoppertweaks.mixin.mods;
+
+import leviathan143.loottweaker.common.mutable_loot.MutableLootTable;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = MutableLootTable.class, remap = false)
+public class MixinMutableLootTable {
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;error(Ljava/lang/String;" +
+            "Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;)V"),
+            method = "<init>(Lnet/minecraft/world/storage/loot/LootTable;Lnet/minecraft/util/ResourceLocation;)V")
+    private void dimhoppertweaks$noMoreSanityChecks(Logger instance, String s, Object o, Object o1, Object o2) {}
+}
