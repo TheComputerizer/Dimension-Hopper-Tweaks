fe872a0bee23fa57c64ac60a612cff506c32651e
TheComputerizer <dcpixelation42@gmail.com>
2023-09-27 20:42:05 -0500

    Remove log spam for hotloading dimension errors

---
+++ src/main/java/mods/thecomputerizer/dimhoppertweaks/mixin/vanilla/MixinDimensionManager.java
@@ -0,0 +1,15 @@
+package mods.thecomputerizer.dimhoppertweaks.mixin.vanilla;
+
+import net.minecraftforge.common.DimensionManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = DimensionManager.class, remap = false)
+public class MixinDimensionManager {
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;" +
+            "error(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"), method = "initDimension")
+    private static void dimhoppertweaks$ignoreErroringDimensions(Logger logger, String s, Object o, Object o1) {}
+}
