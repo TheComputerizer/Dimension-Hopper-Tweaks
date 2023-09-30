3c1716a1d1333b68e8ed1dcefc900fcda701bd2d
TheComputerizer <dcpixelation42@gmail.com>
2023-09-29 19:56:54 -0500

    Fix the XU2 mechanical miner

--- src/main/java/mods/thecomputerizer/dimhoppertweaks/mixin/access/ContainerAccess.java
+++
@@ -1,8 +0,0 @@
-package mods.thecomputerizer.dimhoppertweaks.mixin.access;
-
-import java.util.Collection;
-
-public interface ContainerAccess {
-
-    Collection<String> dimhoppertweaks$getStages();
-}
