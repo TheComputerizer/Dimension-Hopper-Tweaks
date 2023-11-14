e3a8caf88c78e1cbc2585c14ad9c22253bf2b1d4
TheComputerizer <dcpixelation42@gmail.com>
2023-11-13 18:31:12 -0600

    Fix choke mobs breaking air limits and wolf taming not respecting max health

---
+++ src/main/java/mods/thecomputerizer/dimhoppertweaks/mixin/mods/MixinMM_Choke.java
@@ -0,0 +1,32 @@
+package mods.thecomputerizer.dimhoppertweaks.mixin.mods;
+
+import atomicstryker.infernalmobs.common.InfernalMobsCore;
+import atomicstryker.infernalmobs.common.mods.MM_Choke;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(value = MM_Choke.class, remap = false)
+public class MixinMM_Choke {
+
+    @Shadow private EntityLivingBase lastTarget;
+
+    @Shadow private int lastAir;
+
+    /**
+     * @author The_Computerizer
+     * @reason Fix air being able to go past max value
+     */
+    @Overwrite
+    private void updateAir() {
+        this.lastTarget.setAir(this.lastAir);
+        if(this.lastTarget instanceof EntityPlayerMP) {
+            InfernalMobsCore.instance().sendAirPacket((EntityPlayerMP)this.lastTarget,MathHelper.clamp(this.lastAir,0,300));
+            InfernalMobsCore.instance().getModifiedPlayerTimes().put(this.lastTarget.getName(),System.currentTimeMillis());
+        }
+
+    }
+}
---
+++ src/main/java/mods/thecomputerizer/dimhoppertweaks/mixin/vanilla/MixinEntityWolf.java
@@ -0,0 +1,27 @@
+package mods.thecomputerizer.dimhoppertweaks.mixin.vanilla;
+
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.passive.EntityTameable;
+import net.minecraft.entity.passive.EntityWolf;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(EntityWolf.class)
+public abstract class MixinEntityWolf extends EntityTameable {
+
+    public MixinEntityWolf(World world) {
+        super(world);
+    }
+
+    @ModifyConstant(constant = @Constant(floatValue = 20f, ordinal = 0), method = "processInteract")
+    private float dimhoppertweaks$uncapHealing(float constant) {
+        return (float)this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).getAttributeValue();
+    }
+
+    @ModifyConstant(constant = @Constant(floatValue = 20f, ordinal = 1), method = "processInteract")
+    private float dimhoppertweaks$uncapTamedHealth(float constant) {
+        return (float)this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).getAttributeValue();
+    }
+}
