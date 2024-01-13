588ab8eb30df15007573cfea5b2f6234128c026a
TheComputerizer <dcpixelation42@gmail.com>
2024-01-12 20:30:24 -0600

    Fix summoner boss and shockwave sword for real this time

---
+++ src/main/java/mods/thecomputerizer/dimhoppertweaks/mixin/mods/thebetweenlands/MixinItemShockwaveSword.java
@@ -0,0 +1,25 @@
+package mods.thecomputerizer.dimhoppertweaks.mixin.mods.thebetweenlands;
+
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import thebetweenlands.common.item.tools.ItemBLSword;
+import thebetweenlands.common.item.tools.ItemShockwaveSword;
+
+import javax.annotation.Nonnull;
+
+@Mixin(value = ItemShockwaveSword.class, remap = false)
+public abstract class MixinItemShockwaveSword extends ItemBLSword {
+    public MixinItemShockwaveSword(ToolMaterial material) {
+        super(material);
+    }
+
+    /**
+     * @author The_Computerizer
+     * @reason No more damaged shockwave swords
+     */
+    @Overwrite
+    public void setDamage(@Nonnull ItemStack stack, int damage) {
+        super.setDamage(stack,stack.getMaxDamage());
+    }
+}
