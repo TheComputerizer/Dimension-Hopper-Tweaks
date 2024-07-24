68bba2de3429bee27a5f326f3b386afea3dd788f
TheComputerizer <dcpixelation42@gmail.com>
2024-07-24 16:12:46 -0500

    Buff bedrock material by 250x

--- src/main/java/mods/thecomputerizer/dimhoppertweaks/mixin/mods/lockyzextradimensionsmod/MixinFlintChunkProvider.java
+++ src/main/java/mods/thecomputerizer/dimhoppertweaks/mixin/mods/lockyzextradimensionsmod/MixinFlintChunkProvider.java
@@ -1,6 +1,5 @@
 package mods.thecomputerizer.dimhoppertweaks.mixin.mods.lockyzextradimensionsmod;
 
-import com.kashdeya.tinyprogressions.inits.TechBlocks;
 import mod.mcreator.mcreator_dimensionflint;
 import mod.mcreator.mcreator_flintOre;
 import mods.thecomputerizer.dimhoppertweaks.mixin.api.IBlockFlintOre;
@@ -12,15 +11,17 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
+import static com.kashdeya.tinyprogressions.inits.TechBlocks.flint_block;
+
 @SuppressWarnings("unused")
 @Mixin(value = mcreator_dimensionflint.ChunkProviderModded.class, remap = false)
 public abstract class MixinFlintChunkProvider {
 
-    @Final @Shadow protected final static IBlockState NETHERRACK = TechBlocks.flint_block.getDefaultState();
-    @Final @Shadow protected final static IBlockState GRAVEL = TechBlocks.flint_block.getDefaultState();
-    @Final @Shadow protected final static IBlockState SOUL_SAND = TechBlocks.flint_block.getDefaultState();
+    @Final @Shadow protected final static IBlockState NETHERRACK = flint_block.getDefaultState();
+    @Final @Shadow protected final static IBlockState GRAVEL = flint_block.getDefaultState();
+    @Final @Shadow protected final static IBlockState SOUL_SAND = flint_block.getDefaultState();
     @Final @Shadow private final WorldGenerator quartzGen = new WorldGenMinable(((IBlockFlintOre)mcreator_flintOre.block)
-            .dimhoppertweaks$getDefaultState(),14,BlockMatcher.forBlock(TechBlocks.flint_block));
-    @Final @Shadow private final WorldGenerator magmaGen = new WorldGenMinable(TechBlocks.flint_block.getDefaultState(),
-            14,BlockMatcher.forBlock(TechBlocks.flint_block));
+            .dimhoppertweaks$getDefaultState(),14,BlockMatcher.forBlock(flint_block));
+    @Final @Shadow private final WorldGenerator magmaGen = new WorldGenMinable(flint_block.getDefaultState(),
+                                14,BlockMatcher.forBlock(flint_block));
 }
